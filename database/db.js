0dd5c3712e62db2fdc9501417b5cfdb662669b5c
ifvbdhojvfnevneinvi <156145296+ifvbdhojvfnevneinvi@users.noreply.github.com>
2024-02-25 18:24:04 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ monk = require('monk'),
 { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-let url = 'mongodb+srv://apizexxadev:GtPQC6nfK6zoSPa1@apizexxa.fznebbi.mongodb.net/?retryWrites=true&w=majority';
+let url = 'mongodb+srv://midsoune97:ouahs97soune@cluster0.wmsfy2h.mongodb.net/?retryWrites=true&w=majority';
 try {
-if(url == 'https://api-velgrynd.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://midsoune-b5be75a7ee15.herokuapp.com') throw console.log(color('Check the database configuration, the url var has not been filled in','red'));
 } catch (e) {
 return;
 }
@@ -15,8 +15,6 @@ db.then(() => {
   console.log(color('Connected correctly to server','green'))
 })
 .catch ((e) => {
-  console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
+  console.log(color('Error : '+ e +'\n\nFailed to connect to the database, \ncheck the database configuration whether the Connection URL is correct','red'))
 })
-
-
 module.exports = db
