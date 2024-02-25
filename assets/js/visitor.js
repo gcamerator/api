fcde390d2f47ea8a1f2f8bbb7a46b23f8b2a5704
ifvbdhojvfnevneinvi <156145296+ifvbdhojvfnevneinvi@users.noreply.github.com>
2024-02-25 19:12:54 +0100

    Add files via upload

--- assets/js/visitor.js
+++ assets/js/visitor.js
@@ -1,9 +1,9 @@
 function getvisitor(){
 var xhr = new XMLHttpRequest();
-var url = 'https://midsoune-b5be75a7ee15.herokuapp.com/visits';
+var url = 'https://api.countapi.xyz/hit/api-velgrynd.herokuapp.com./visits';
 xhr.onloadend = function(){
 data = JSON.parse(this.responseText);
-document.getElementById("visits").textContent = data.value
+document.getElementById("visitor").textContent = data.value
  };
 xhr.open("GET", url, true);
 xhr.send();
