41632528eb68f155ecbac9b2410806076e83c46a
ifvbdhojvfnevneinvi <156145296+ifvbdhojvfnevneinvi@users.noreply.github.com>
2024-02-25 17:26:31 +0100

    Update visitor.js

--- assets/js/visitor.js
+++ assets/js/visitor.js
@@ -3,7 +3,7 @@ var xhr = new XMLHttpRequest();
 var url = 'https://midsoune-b5be75a7ee15.herokuapp.com/visits';
 xhr.onloadend = function(){
 data = JSON.parse(this.responseText);
-document.getElementById("visitor").textContent = data.value
+document.getElementById("visits").textContent = data.value
  };
 xhr.open("GET", url, true);
 xhr.send();
