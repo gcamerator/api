322e2c439f3988719cfe9e93a4e89f359ba4ad0d
Midsoune@24 <165938418+gcamerator@users.noreply.github.com>
2024-05-24 20:12:18 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,20 +1,49 @@
-let __path = process.cwd(),
-monk = require('monk'),
-{ color } = require(__path + '/lib/color.js')
+const monk = require('monk');
+const { color } = require('../lib/color.js');
 
-// Connection URL
-let url = 'mongodb+srv://midsoune97:ouahs97soune@cluster0.wmsfy2h.mongodb.net/?retryWrites=true&w=majority';
-try {
-if(url == 'https://midsouneapi-fee7b0be8faf.herokuapp.com') throw console.log(color('Check the database configuration, the url var has not been filled in','red'));
-} catch (e) {
-return;
-}
-let db = monk(url);
+const url = 'mongodb+srv://midsoune97:ouahs97soune@cluster0.wmsfy2h.mongodb.net/?retryWrites=true&w=majority';
+const db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server','green'))
-})
-.catch ((e) => {
-  console.log(color('Error : '+ e +'\n\nFailed to connect to the database, \ncheck the database configuration whether the Connection URL is correct','red'))
-})
-module.exports = db
+  console.log(color('Connected correctly to server', 'green'));
+}).catch((e) => {
+  console.log(color('Error: ' + e, 'red'));
+});
+
+// const moutamadrisCollection = db.get('moutamadris');
+
+const initialData = {
+  currlien: 'https://moutamadris.ma/cours/',
+  currMsg: '🗄️ *إختر مستواك الدراسي من القائمة التالية:*\n\n*1. الثانية باك*\n*2. اولى باك*\n*3. الجذع مشترك*\n*4. الثالثة اعدادي*\n*5. الثانية اعدادي*\n*6. الأولى اعدادي*\n*7. السادس ابتدائي*\n*8. الخامس ابتدائي*\n*9. الرابع ابتدائي*\n*10. الثالث ابتدائي*\n*11. الثاني ابتدائي*\n*12. الأول ابتدائي*\n*13. دروس ملخصات وتمارين خيار فرنسية وانجليزية*\n\n*0. العودة للصفحة السابقة*',
+  links: [],
+  msgs: [],
+  newmsg: null,
+  currstep: 0,
+  chosen: 0,
+  mada: 0,
+  add: 0,
+  max: 13,
+  download: 0,
+  student: null,
+  content: []
+};
+
+async function seedDatabase() {
+  try {
+    const existingData = await moutamadrisCollection.findOne({});
+    if (existingData) {
+      console.log(color('Data already exists in the collection. Skipping insertion.', 'yellow'));
+    } else {
+      await moutamadrisCollection.insert(initialData);
+      console.log(color('Initial data inserted successfully into moutamadris collection.', 'green'));
+    }
+  } catch (error) {
+    console.error(color('Error inserting initial data:', 'red'), error);
+  } finally {
+    db.close(); // تأكد من إغلاق الاتصال بقاعدة البيانات بعد الانتهاء
+  }
+}
+
+// seedDatabase();
+
+module.exports = db;
